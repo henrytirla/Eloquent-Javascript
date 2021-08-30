824f8d5461c081777b5de1511e5be33ab90672ed
henrytirla <henrytirla@gmail.com>
2021-08-30 16:56:30 +0100

    Beancounting js

---
+++ Chapter3_Exercise/BeanCounting.js
@@ -0,0 +1,28 @@
+
+
+function countBs(str){
+let count = 0;
+
+  for (var i = 0 ; i < str.length; i++){
+
+    if (str[i] >= "A" && str[i] <= "Z") count++;
+
+  }
+  return count;
+}
+
+
+
+function countChar(str,letter){
+  let count = 0;
+  for (var i = 0 ; i < str.length; i++){
+
+    if (str[i] == letter) {count++;}
+
+
+  }
+  return count;
+}
+
+console.log(countBs("BaBy","B"))
+//console.log(countBs("LoVe"));
