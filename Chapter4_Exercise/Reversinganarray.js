2c28516af8bee4664d078aa369c1d80d1fa611c6
henrytirla <henrytirla@gmail.com>
2021-12-02 03:35:37 +0100

    completed

--- Chapter4_Exercise/Reversinganarray.js
+++ Chapter4_Exercise/Reversinganarray.js
@@ -17,20 +17,21 @@ know that reversing in array simply means reversing without using another array
 
    **/
 
-// function reverseArray(arr){
-// let   arr_length = arr.length;
-// let   new_arr =[];
-//   for(let i=1;i<=arr_length;i++){
-//
-//     new_arr.push(arr[arr_length-i]);
-// }
-//   console.log(new_arr);
-//   }
-//
-// reverseArray([1,2,3,4,5])
+function reverseArray(arr){
+let   arr_length = arr.length;
+let   new_arr =[];
+  for(let i=1;i<=arr_length;i++){
+
+    new_arr.push(arr[arr_length-i]);
+}
+  console.log(new_arr);
+  }
+
+reverseArray([1,2,3,4,5])
 
 
-
+// Useful explanation link
+//https://www.youtube.com/watch?v=5i9tQjFXUO4
 function reverseArrayInPlace(arr2){
 
    let temp
@@ -41,21 +42,11 @@ function reverseArrayInPlace(arr2){
      arr2[arr2.length-1-i] = temp
 
 
-    console.log(i,temp, arr2[i]);
+
     //arr2[n-1] = temp;
   }
-  //return arr2;
-  console.log(i,temp, arr2[i]);
-
-
-     //return popped;
-     //arr2.push(popped)
-     //console.log(start);
-
-
-
-
+  return arr2;
   }
 
 
-console.log(reverseArrayInPlace([1,2,3,4]));
+console.log(reverseArrayInPlace([1,2,3,4,5]));
